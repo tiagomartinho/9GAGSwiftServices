068c3a3046d3274ecc3db68d7df5fc9b0f2a7970
tiagomartinho <tiago.martinho@xpeppers.com>
2016-07-09 14:15:31 +0200

    adds public init to Item

--- services/Services/Item.swift
+++ services/Services/Item.swift
@@ -1,4 +1,9 @@
 public struct Item {
     public let caption: String
     public let image: String
+
+    public init(caption: String, image: String) {
+        self.caption = caption
+        self.image = image
+    }
 }
